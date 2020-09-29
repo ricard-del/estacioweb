3be179a7907497092185eba6fa2949ea6fbf8154
ricard-del <70780953+ricard-del@users.noreply.github.com>
2020-09-28 23:56:06 -0300

    alterações do programa

---
+++ formularioexercicio18092020/exerciciopag90.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = $_POST["nome"];
+$matricula = $_POST["matricula"];
+$email = $_POST["email"];
+$telefone = $_POST["telefone"];
+$numerodesap = $_POST["numerodesap"];
+echo "Nome: ".$nome." matricula: ".$matricula. "email:" .$email. "telefone:" .$telefone. "numero de sap:" .$numerodesap;
+
+?>
+<p> <a href="exerciciopag90.html">Voltar</a>  </p>
+
